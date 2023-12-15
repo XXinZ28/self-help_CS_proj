80f3ec946a62895cd653158a75a3dc3e61c2c8c8
XXinZ28 <113189663+XXinZ28@users.noreply.github.com>
2023-12-15 10:28:46 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# ------------------------------------------------------
+# Name: - smith student check list
+# Peers: - Doris Chi and Xinxin Zhang
+# References: - N/A
+# Use of generative AI (if any):
+# ------------------------------------------------------
+"""
+Import the modules being used in this prpject.
+"""
+import myprint
+import buttons
+import summary
+import graphics
+
+
+def main():
+ 
+  myprint.main()
+  buttons.main()
+  summary.main()
+
+
+if __name__ == "__main__":
+  main()
+
